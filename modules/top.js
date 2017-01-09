f29bc8244bff560498ac40e7ef2833bba87fdb40
Bryan T. Meyers <bmeyers@datadrake.com>
2017-01-09 16:57:32 -0500

    Improved simple-top with hiding and onscroll()

--- modules/top.js
+++ modules/top.js
@@ -2,6 +2,16 @@
  MIT License
  Copyright (c) 2016 Bryan T. Meyers
  */
+window.onload = function () {
+    u('a[simple-top]').first().style = "display: none;";
+};
+window.onscroll = function() {
+    if (window.scrollY >= 50 ){
+        u('a[simple-top]').first().style = "display: '';";
+    } else {
+        u('a[simple-top]').first().style = "display: none;";
+    }
+};
 u('a[simple-top]').on('click', function() {
-    u(body).scroll()
+    u('body').scroll()
 });
